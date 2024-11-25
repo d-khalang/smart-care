5fbdb2e1a82f7cd1f66fe7707c9a203440a86b50
Dani khalang <danikhalang@gmail.com>
2024-11-25 17:17:21 +0100

    status code added to generate response

--- utility.py
+++ utility.py
@@ -2,5 +2,13 @@ def convert_to_bool(param):
     if param and param.lower() in ["true", "1"]:
         return bool(param)
 
-def create_response(success: bool, content=None, message=None):
-    return {"success": success, "content": content, "message": message}
+def create_response(success: bool, content: dict = None, message: str = "", status: int = 200) -> dict:
+    response = {
+        "success": success,
+        "status": status,
+    }
+    if content is not None:
+        response["content"] = content
+    if message:
+        response["message"] = message
+    return response
