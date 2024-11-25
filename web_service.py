e202bfbcb934352db3f81a2a54f66c1a985dd961
Dani khalang <danikhalang@gmail.com>
2024-11-25 17:19:22 +0100

    have way through post

--- web_service.py
+++ web_service.py
@@ -11,13 +11,15 @@ class WebCatalog():
     @cherrypy.tools.json_in()
     def GET(self, *uri, **params):
         if len(uri) < 1:
-            return create_response(False, message="No url inserted, try from 'general', 'plants', 'devices', ...")
+            return create_response(False, message="No url inserted, try from 'general', 'plants', 'devices', plant_kinds, ...")
         return self.handler.handle_get(uri, params)
 
     @cherrypy.tools.json_out()
     @cherrypy.tools.json_in()
     def POST(self, *uri, **params):
         data = cherrypy.request.json
+        if len(uri) < 1:
+            return create_response(False, message="No url inserted, try from 'plants', 'devices', ...")
         return self.handler.handle_post(uri, params, data)
 
     @cherrypy.tools.json_out()
