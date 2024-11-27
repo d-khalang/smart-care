0e76439448fdebea3a99b4c6aa8830d33760daf4
Dani khalang <danikhalang@gmail.com>
2024-11-27 15:31:04 +0100

    methods separated from config

--- web_service.py
+++ web_service.py
@@ -1,15 +1,16 @@
 import cherrypy
-from handlers import Handlers
 from utility import create_response
+from handlers import Handler
 
 class WebCatalog():
     exposed = True
-    def __init__(self) -> None:
-        self.handler = Handlers()
+    def __init__(self, handler: Handler) -> None:
+        self.handler = handler
 
     @cherrypy.tools.json_out()
     @cherrypy.tools.json_in()
     def GET(self, *uri, **params):
+        print({"uri":uri, "param":params})
         if len(uri) < 1:
             return create_response(False, message="No url inserted, try from 'general', 'plants', 'devices', plant_kinds, ...")
         return self.handler.handle_get(uri, params)
@@ -26,22 +27,15 @@ class WebCatalog():
     @cherrypy.tools.json_in()
     def PUT(self, *uri, **params):
         data = cherrypy.request.json
+        if len(uri) < 1:
+            return create_response(False, message="No url inserted, try from 'plants', 'devices', ...")
         return self.handler.handle_put(uri, params, data)
 
     @cherrypy.tools.json_out()
     @cherrypy.tools.json_in()
     def DELETE(self, *uri, **params):
+        if len(uri) < 1:
+            return create_response(False, message="No url inserted, try from 'plants', 'devices', ...")
         return self.handler.handle_delete(uri, params)
 
 
-if __name__ == '__main__':
-    conf = {
-        "/": {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.sessions.on': True
-        }
-    }
-    webService = WebCatalog()
-    cherrypy.tree.mount(webService, '/', conf)
-    cherrypy.engine.start()
-    cherrypy.engine.block()
