2cda3a4a3bd0eb8697e7cacab18576bf5839f905
Dani khalang <danikhalang@gmail.com>
2024-12-02 18:01:17 +0100

    mqtt againt, data preparation and publisher methods added

--- device_connector/device_connector.py
+++ device_connector/device_connector.py
@@ -2,30 +2,74 @@ import os
 import json
 import requests
 import time
-import sched
+import threading
+import copy
 from typing import Literal
 from models import Device, Plant
 from config import Config, MyLogger
+from sensors import TempSen, LightSen, PHSen, SoilMoistureSen, create_sensor
+from utility import case_insensitive
+from MyMQTT2 import MyMQTT
+
+
+class MyClientMQTT():
+    def __init__(self, clientID, broker, port, host, child_logger):
+        self.host = host
+        self.client = MyMQTT(clientID, broker, port, self, child_logger)
+        self.start()  
+
+    ## Connecting to the broker
+    def start(self):
+        self.client.start()
+
+    ## Disconnecting from the broker
+    def stop(self):
+        self.client.stop()
+
+    def publish(self, topic, msg):
+        self.client.myPublish(topic, msg)
+
+    # Add the topic to the subscribed topics not substitude
+    def subscribe(self, topic):
+        self.client.mySubscribe(topic)
+    
+    # Will be triggered when a message is received
+    def notify(self, topic, payload):
+        self.host.notify(topic, payload)
+
 
 
 class DeviceConnector:
-    def __init__(self, config_file: str, catalog_address: str):
+    def __init__(self, config: Config):
+        self.config = config
         self.devices = []
         self.plants = []
-        self.catalog_address = catalog_address
+        self.available_sensors = {}
+        self.catalog_address = self.config.CATALOG_URL
         self.logger = MyLogger.get_main_loggger()
-        self._initiate(config_file=config_file)
-        # # Scheduler config
-        # self.run_scheduler()
+        self.broker = None
+        self.port = None
+        self.msg = {
+            "bn": "",
+            "e": [
+                {
+                    "n": 'senKind',
+                    "u": 'unit',
+                    "t": None,
+                    "v": None
+                }
+            ]
+        }
+
+        self._initiate(config_file=self.config.CONFIG_FILE)
+        self.get_broker()
+        self._initiate_mqtt()
+        self.initialize_sensors() # Initialize sensors after loading devices
+        self.start_data_collection_thread()  # Start data collection in a separate thread
+
         
 
     # TODO: serivice registry    
-
-    # def run_scheduler(self):
-    #     self.registration_interval = Config.REGISTERATION_INTERVAL
-    #     self.scheduler = sched.scheduler(time.time, time.sleep)
-    #     self.scheduler.enter(self.registration_interval, 1, self.register, ())
-    #     self.scheduler.run(blocking=False)
 
 
     def _initiate(self, config_file: str):
@@ -59,6 +103,39 @@ class DeviceConnector:
         self.register(initial=True)
 
 
+
+    def _initiate_mqtt(self):
+        self.mqtt_client = MyClientMQTT(clientID = self.config.MQTT_CLIENT_ID,
+                                        broker=self.broker,
+                                        port=self.port,
+                                        host=self,
+                                        child_logger=MyLogger.set_logger(logger_name=Config.MQTT_LOGGER))
+
+
+    def initialize_sensors(self):
+        self.logger.info("Initializing sensors...")
+        for device in self.devices:
+            if device.device_type == "sensor":
+                for measure_type in device.measure_types:
+                    try:
+                        sensor_instance = create_sensor(measure_type)
+                        sensor_key = f"{measure_type}_{device.device_id}"
+                        self.available_sensors[sensor_key] = {"obj": sensor_instance, "topics": []}
+                        self.logger.info(f"Sensor {sensor_key} created and added.")
+
+                        for service_detail in device.services_details:
+                            if service_detail.service_type == "MQTT":
+                                for topic in service_detail.topic:
+                                    if measure_type in topic:
+                                        self.available_sensors[sensor_key]["topics"].append(topic)
+
+                    except ValueError as e:
+                        self.logger.error(f"Error creating sensor for device {device.device_id} with type {measure_type}: {e}")
+                
+                
+        self.logger.debug(f"Created sensors: {self.available_sensors}")
+
+
     def add_device(self, device: Device):
         self.devices.append(device)
         self.logger.info(f"Device {device.device_id} added.")
@@ -80,7 +157,7 @@ class DeviceConnector:
 
 
     def _register_plants(self, initial: bool=False):
-        url = f"{self.catalog_address}/{Config.PLANTS_ENDPOINT}"
+        url = f"{self.catalog_address}/{self.config.PLANTS_ENDPOINT}"
         method = "POST" if initial else "PUT"
         for plant in self.plants:
             self.logger.info(f"Registring plant {plant.plant_id} ...")
@@ -89,7 +166,7 @@ class DeviceConnector:
 
 
     def _register_devices(self, initial: bool=False):
-        url = f"{self.catalog_address}/{Config.DEVICES_ENDPOINT}"
+        url = f"{self.catalog_address}/{self.config.DEVICES_ENDPOINT}"
         method = 'POST' if initial else 'PUT'
         for device in self.devices:
             self.logger.info(f"Registring device {device.device_id} ...")
@@ -110,27 +187,135 @@ class DeviceConnector:
             response.raise_for_status()
             if not response.json().get("success"):
                 self.logger.info(f"Registration unsuccessful for {item_type} {item_id} with message {str(response.text)}.")
-
-            self.logger.info(f"Successfully registered {item_type} {item_id}.")
+            else:
+                self.logger.info(f"Successfully registered {item_type} {item_id}.")
 
         except requests.RequestException as e:
             self.logger.error(f"Failed to register {item_type} {item_id}: {e}")
 
 
+    def collect_and_average_sensor_data(self):
+        while True:
+            collected_data = {device_id: [] for device_id in self.available_sensors.keys()}
+            
+            try:
+                for _ in range(self.config.DATA_POINTS_FOR_AVERAGE):
+                    for device_id, sensor_dict in self.available_sensors.items():
+                        sensor = sensor_dict.get("obj")
+                        if sensor:
+                            try:
+                                data = sensor.generate_data()
+                                collected_data[device_id].append(data)
+                            except Exception as e:
+                                self.logger.error(f"Error generating data for sensor {device_id}: {e}")
+                        else:
+                            self.logger.error(f"Sensor object not found for device {device_id}")
+                    time.sleep(self.config.DATA_COLLECTION_INTERVAL)
+
+                averaged_data = {}
+                for device_id, data_list in collected_data.items():
+                    if data_list:
+                        averaged_data[device_id] = sum(data_list) / len(data_list)
+                    else:
+                        self.logger.warning(f"No data collected for device {device_id}")
+
+                self.logger.info(f"Averaged sensor data: {averaged_data}")
+                self.prepare_data_to_publish(averaged_data)
+
+            except Exception as e:
+                self.logger.error(f"Error in data collection loop: {e}")
+
+
+    def start_data_collection_thread(self):
+        # Start the data collection in a separate thread so it doesn't block the main thread
+        try:
+            data_collection_thread = threading.Thread(target=self.collect_and_average_sensor_data)
+            data_collection_thread.daemon = True  # Ensures the thread is killed when the main program exits
+            data_collection_thread.start()    
+        except Exception as e:
+            self.logger.error(f"Error starting data collection thread: {e}")
+
+
+    def prepare_data_to_publish(self, averaged_data: dict):
+        for sensor_key, datum in averaged_data.items():
+            sensor_dict = self.available_sensors.get(sensor_key)
+            if sensor_dict:
+                sensor = sensor_dict.get("obj")
+                if sensor:
+                    try:
+                        msg = copy.deepcopy(self.msg)
+                        msg['e'][0]['n'], msg['e'][0]['u'] = sensor.get_info()
+                        msg['e'][0]['t'] = str(time.time())
+                        msg['e'][0]['v'] = datum
+
+                        for topic in sensor_dict.get("topics"):
+                            msg['bn'] = topic
+                            try:
+                                self.mqtt_client.publish(topic=topic, msg=msg)
+                                self.logger.info(f"Message {datum} published on topic: {topic}")
+                            except Exception as e:
+                                self.logger.error(f"Error publishing message to topic {topic}: {e}")
+
+                    except Exception as e:
+                        self.logger.error(f"Error preparing message for sensor {sensor_key}: {e}")
+                else:
+                    self.logger.error(f"Sensor object not found for sensor {sensor_key}")
+            else:
+                self.logger.error(f"Sensor dictionary not found for sensor {sensor_key}")
+            
+
+
+    # def get_broker(self):
+    #     self.broker = "mqtt.eclipseprojects.io"
+    #     self.port = 1883
+
+    def get_broker(self):
+        try:
+            url = f"{self.catalog_address}/general/broker"
+            self.logger.info(f"Fetching broker information from {url} ...")
+            response = requests.get(url)
+            response.raise_for_status()
+            broker_response = response.json()
+
+            if broker_response.get("success"):
+                broker_info = broker_response["content"].get("broker")
+
+            self.broker = broker_info.get("IP")
+            self.port = broker_info.get("port")
+
+            if not self.broker or not self.port:
+                raise ValueError("Broker information is incomplete.")
+
+            self.logger.info(f"Broker set to {self.broker} and port set to {self.port}.")
+        except requests.RequestException as e:
+            self.logger.error(f"Failed to fetch broker information: {e}")
+        except ValueError as e:
+            self.logger.error(f"Invalid broker information received: {e}")
 
 
 
-
-
-
+    def notify(self, topic, payload):
+        msg = json.loads(payload)
+        
+        # Part of the message related to the event happened
+        event = msg["e"][0]
+        print(f"{topic} measured a {event['n']} of {event['v']} {event['u']} at time {event['t']}")
 
 if __name__ == "__main__":
-    dc = DeviceConnector(config_file=Config.CONFIG_FILE, catalog_address=Config.CATALOG_URL)
+    dc = DeviceConnector(config=Config)
+    
+    while True:
+        time.sleep(10)
+        print(".........")
+        
+        
+        # dc.collect_and_average_sensor_data()
 
-    t = 1
-    while t < 600:
-        if t%Config.REGISTERATION_INTERVAL == 0:
-            dc.register()
+    # t = 1
+    # while t < 600:
+    #     if t % Config.REGISTERATION_INTERVAL == 0:
+    #         dc.register()
+    #     if t % Config.DATA_GENERATION_INTERVAL ==
 
-        time.sleep(1)
-        t+=1
+    #     time.sleep(1)
+    #     t+=1
