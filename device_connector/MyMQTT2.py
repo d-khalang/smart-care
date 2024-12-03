3896939d91af41dee4ff7e0abfb85466b5ca341a
Dani khalang <danikhalang@gmail.com>
2024-12-03 17:56:42 +0100

    checks added

--- device_connector/MyMQTT2.py
+++ device_connector/MyMQTT2.py
@@ -9,6 +9,7 @@ class MyMQTT:
         self.notifier = notifier
         self.clientID = clientID
         self.logger = child_logger
+
         self._topic = []
         self._isSubscriber = False
         # create an instance of paho.mqtt.client
@@ -18,40 +19,52 @@ class MyMQTT:
         self._paho_mqtt.on_message = self.myOnMessageReceived
 
     def myOnConnect(self, paho_mqtt, userdata, flags, rc):
-        self.logger.info("Connected to %s with result code: %d" % (self.broker, rc))
+        print("Connected to %s with result code: %d" % (self.broker, rc))
 
     def myOnMessageReceived(self, paho_mqtt, userdata, msg):
         # A new message is received
         self.notifier.notify(msg.topic, msg.payload)
 
+
     def myPublish(self, topic, msg):
         # publish a message with a certain topic
-        self._paho_mqtt.publish(topic, json.dumps(msg), 2)
+        try:
+            self._paho_mqtt.publish(topic, json.dumps(msg), qos=2)
+        except Exception as e:
+            print(f"Error publishing message: {e}")
+
 
     def mySubscribe(self, topic):
-
         # subscribe for a topic
-        self._paho_mqtt.subscribe(topic, 2)
-        # just to remember that it works also as a subscriber
-        self._isSubscriber = True
-        self._topic.append(topic)
-        self.logger.info("subscribed to %s" % (topic))
+        try:
+            self._paho_mqtt.subscribe(topic, qos=2)
+            self._isSubscriber = True
+            self._topic.append(topic)
+            print(f"Subscribed to {topic}")
+            
+        except Exception as e:
+            print(f"Error subscribing to topic {topic}: {e}")
+
 
     def start(self):
         # manage connection to broker
-        self._paho_mqtt.connect(self.broker, self.port)
-        self._paho_mqtt.loop_start()
+        try:
+            self._paho_mqtt.connect(self.broker, self.port)
+            self._paho_mqtt.loop_start()
+        except Exception as e:
+            print(f"Error starting MQTT client: {e}")
+
 
     def unsubscribe(self, topic):
-        if (self._isSubscriber):
-            # remember to unsuscribe if it is working also as subscriber
-            self._paho_mqtt.unsubscribe(topic)
+        if self._isSubscriber:
+            try:
+                self._paho_mqtt.unsubscribe(topic)
+            except Exception as e:
+                print(f"Error unsubscribing from topic {topic}: {e}")
 
     def stop(self):
-        if (self._isSubscriber):
-            # remember to unsuscribe if it is working also as subscriber
+        if self._isSubscriber:
             for topic in self._topic:
-                self._paho_mqtt.unsubscribe(topic)
-
+                self.unsubscribe(topic)
         self._paho_mqtt.loop_stop()
         self._paho_mqtt.disconnect()
